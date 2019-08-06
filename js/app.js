29e5a95cd889f96365b83fbe86e470ffc51760ff
Dexter De Leon <dexdeleon@gmail.com>
2019-08-06 16:04:51 +0100

    JavaScript refactor.

--- js/app.js
+++ js/app.js
@@ -1,30 +1,41 @@
-document.addEventListener('DOMContentLoaded', () => {
+let buttons,
+  modal,
+  exitButton,
+  mailButton,
+  modalHeader
 
-  const buttons = document.querySelectorAll('.callback')
-  const modal = document.querySelector('.modal-overlay')
-  const exitButton = document.querySelector('.exit')
-  const mailButton = document.getElementById('mail-button')
-  const modalHeader = document.querySelector('.modal h2')
+function init(){
+  buttons = document.querySelectorAll('.callback')
+  modal = document.querySelector('.modal-overlay')
+  exitButton = document.querySelector('.exit')
+  mailButton = document.getElementById('mail-button')
+  modalHeader = document.querySelector('.modal h2')
 
-  function handleButton(button){
-    const spaceIndex = button.value.indexOf(' ')
-    const offerType = button.value.substring(0, spaceIndex)
-    modalHeader.innerText = `Requesting a callback about a${offerType.charAt(0) === 'E'? 'n':''} ${button.value}`
-    mailButton.setAttribute(
-      'href',
-      `mailto:info@acquio.co.uk?subject=${offerType} Partner Offer`)
+  addEvents()
+}
+
+function handleButton(button){
+  const spaceIndex = button.value.indexOf(' ')
+  const offerType = button.value.substring(0, spaceIndex)
+  modalHeader.innerText = `Requesting a callback about a${offerType.charAt(0) === 'E'? 'n':''} ${button.value}`
+  mailButton.setAttribute(
+    'href',
+    `mailto:info@acquio.co.uk?subject=${offerType} Partner Offer`
+  )
+}
+
+function handleModal(button){
+  modal.classList.toggle('displayed')
+  if (button.value){
+    handleButton(button)
   }
+}
 
-  function handleModal(button){
-    modal.classList.toggle('displayed')
-    if (button.value){
-      handleButton(button)
-    }
-  }
-
+function addEvents(){
   buttons.forEach(button => {
     button.addEventListener('click', () => handleModal(button))
   })
-
   exitButton.addEventListener('click', () => handleModal(exitButton))
-})
+}
+
+document.addEventListener('DOMContentLoaded', init)
