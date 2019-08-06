5748b315d2c89d7ef6c7cd05f3ae83e998423a15
Dexter De Leon <dexdeleon@gmail.com>
2019-08-06 01:01:19 +0100

    JavaScript for modal open and close.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   const buttons = document.querySelectorAll('.callback')
+  const modal = document.querySelector('.modal-overlay')
+  const exitButton = document.querySelector('.exit')
+
+  function handleModal(){
+    modal.classList.toggle('displayed')
+  }
+
 
   buttons.forEach(button => {
-    button.addEventListener('click', () => {
-      console.log(button.value)
-    })
+    button.addEventListener('click', handleModal)
   })
+
+  exitButton.addEventListener('click', handleModal)
 
   console.log('🏃🏽‍♂️💨')
 })
