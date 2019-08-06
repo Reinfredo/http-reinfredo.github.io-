c7a630e6a53b95b66c935cc4b2f7dc26ccca54d9
Dexter De Leon <dexdeleon@gmail.com>
2019-08-06 15:51:19 +0100

    JavaScript handling button and modal added.

--- js/app.js
+++ js/app.js
@@ -3,17 +3,28 @@ document.addEventListener('DOMContentLoaded', () => {
   const buttons = document.querySelectorAll('.callback')
   const modal = document.querySelector('.modal-overlay')
   const exitButton = document.querySelector('.exit')
+  const mailButton = document.getElementById('mail-button')
+  const modalHeader = document.querySelector('.modal h2')
 
-  function handleModal(){
-    modal.classList.toggle('displayed')
+  function handleButton(button){
+    const spaceIndex = button.value.indexOf(' ')
+    const offerType = button.value.substring(0, spaceIndex)
+    modalHeader.innerText = `Requesting a callback about a${offerType.charAt(0) === 'E'? 'n':''} ${button.value}`
+    mailButton.setAttribute(
+      'href',
+      `mailto:info@acquio.co.uk?subject=${offerType} Partner Offer`)
   }
 
+  function handleModal(button){
+    modal.classList.toggle('displayed')
+    if (button.value){
+      handleButton(button)
+    }
+  }
 
   buttons.forEach(button => {
-    button.addEventListener('click', handleModal)
+    button.addEventListener('click', () => handleModal(button))
   })
 
-  exitButton.addEventListener('click', handleModal)
-
-  console.log('🏃🏽‍♂️💨')
+  exitButton.addEventListener('click', () => handleModal(exitButton))
 })
